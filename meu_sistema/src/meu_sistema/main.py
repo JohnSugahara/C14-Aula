8baf0f77c5d0512f274e9e9fa63f11ae83e88e70
JohnSugahara <john.sugahara@hotmail.com.br>
2025-08-18 19:52:24 -0300

    Primeiro Commmit

---
+++ meu_sistema/src/meu_sistema/main.py
@@ -0,0 +1,21 @@
+import requests
+
+def main():
+    pokemon = input("Digite o nome de um Pokémon: ")
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon.lower()}"
+
+    resposta = requests.get(url)
+
+    if resposta.status_code == 200:
+        dados = resposta.json()
+        print(f"\nNome: {dados['name'].title()}")
+        print(f"Altura: {dados['height']} dm")
+        print(f"Peso: {dados['weight']} hg")
+        print("Habilidades:")
+        for habilidade in dados["abilities"]:
+            print(f" - {habilidade['ability']['name']}")
+    else:
+        print(f"Erro: Pokémon '{pokemon}' não encontrado.")
+
+if __name__ == "__main__":
+    main()
